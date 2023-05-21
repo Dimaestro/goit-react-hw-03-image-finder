2cf36ef93615fb1661b8e353f4e5bcfbe8b288bf
Dima <vidistudioproduction@gmail.com>
2023-05-21 21:29:35 +0300

    init project image-finder

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import styles from './App.module.css';
+
+
+export class App extends Component {
+
+  render() {
+    return (
+      <div className={styles.app}>
+        
+      </div>
+    )
+  }
+}
